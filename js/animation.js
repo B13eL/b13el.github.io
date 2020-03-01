ec344054676fd0116223cd0c7df6652ec8326e08
B13el <evheiimartynenko@gmail.com>
2020-03-01 10:09:53 +0200

    push korovka

---
+++ js/animation.js
@@ -0,0 +1,36 @@
+let showUp = document.querySelectorAll('[animation="show-up"]'),
+    showLeft = document.querySelectorAll('[animation="show-left"]'),
+    scaleDown = document.querySelectorAll('[animation="scale-down"]'),
+    scale = document.querySelectorAll('[animation="scale"]');
+
+function animateElementsCycle () {
+  for (let i = 0; i < showUp.length; ++i) {
+    if (showUp[i].getBoundingClientRect().top < 600 &&
+    !showUp[i].classList.contains('animation-show-top')) {
+      showUp[i].classList.add('animation-show-top');
+    }
+  }
+  for (let i = 0; i <  showLeft.length; ++i) {
+    if ( showLeft[i].getBoundingClientRect().top < 600 &&
+    !showLeft[i].classList.contains('animation-show-left')) {
+        showLeft[i].classList.add('animation-show-left');
+    }
+  }
+  for (let i = 0; i < scale.length; ++i) {
+    if (scale[i].getBoundingClientRect().top < 600 &&
+    !scale[i].classList.contains('animation-scale')) {
+      scale[i].classList.add('animation-scale');
+    }
+  }
+  for (let i = 0; i < scaleDown.length; ++i) {
+    if (scaleDown[i].getBoundingClientRect().top < 600 &&
+    !scaleDown[i].classList.contains('animation-scale-down')) {
+      scaleDown[i].classList.add('animation-scale-down');
+    }
+  }
+}
+animateElementsCycle();
+
+window.addEventListener('scroll', function () {
+  animateElementsCycle();
+});
